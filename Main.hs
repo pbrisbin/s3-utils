dc8e56fe6fd5d8a5442f3250c3c7d10332c2d6b1
patrick brisbin <pbrisbin@gmail.com>
2011-07-14 16:41:23 -0400

    initial commit

---
+++ Main.hs
@@ -0,0 +1,104 @@
+-- https://github.com/pbrisbin/s3
+module Main where
+
+import Config
+
+import Network.AWS.S3Object
+
+import Control.Exception  (IOException, handle)
+import Control.Monad      (forM)
+import System.Directory   (doesFileExist, doesDirectoryExist, getDirectoryContents)
+import System.Environment (getArgs)
+import System.FilePath    ((</>))
+import System.IO          (hPutStrLn, stderr)
+import Network.Wai.Application.Static (defaultMimeTypeByExt)
+
+import qualified Data.ByteString.Lazy       as B
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Char8      as C8
+
+main :: IO ()
+main = handleArgs =<< getArgs
+
+handleArgs :: [String] -> IO ()
+handleArgs []          = putStrLn "usage: s3 [ --get ] <path> ..."
+handleArgs args@(a:as) =
+    if a == "--get"
+        then mapM_ getArg  as
+        else mapM_ sendArg args
+
+sendArg :: FilePath -> IO ()
+sendArg fp = do
+    isDirectory <- doesDirectoryExist fp
+    if isDirectory
+        then mapM_ sendFile =<< walkDirectory fp
+        else do
+            isFile <- doesFileExist fp
+            if isFile
+                then sendFile fp
+                else return ()
+
+-- AWS will 404 on directory argument
+getArg :: FilePath -> IO ()
+getArg = getFile
+
+sendFile :: FilePath -> IO ()
+sendFile fp = handle skipFile $ do
+    obj <- do
+        fileData <- B.readFile fp
+        return S3Object
+            { obj_bucket   = myBucket
+            , obj_name     = strip fp
+            , content_type = C8.unpack $ defaultMimeTypeByExt fp
+            , obj_headers  = []
+            , obj_data     = fileData
+            }
+
+    resp <- sendObject myConnection obj
+
+    putStrLn $ case resp of
+        Left  e -> show e
+        Right _ -> "Sent " ++ fp
+
+getFile :: FilePath -> IO ()
+getFile fp = handle skipFile $ do
+    resp <- getObject myConnection $ skel fp
+    case resp of
+        Left  e   -> putStrLn $ show e
+        Right obj -> do
+            B.writeFile fp (obj_data obj)
+            putStrLn $ "Created " ++ fp
+
+    where
+        skel :: FilePath -> S3Object
+        skel f = S3Object myBucket (strip f) "" [] (L8.pack "")
+
+strip :: FilePath -> FilePath
+strip ('/':rest)         = strip rest
+strip ('.':'/':rest)     = strip rest
+strip ('.':'.':'/':rest) = strip rest
+strip x                  = x
+
+-- skip unreadable files
+skipFile :: IOException -> IO ()
+skipFile e = hPutStrLn stderr $ show e
+
+walkDirectory :: FilePath -> IO [FilePath]
+walkDirectory dir = handle skipDir $ do
+    names <- getDirectoryContents dir
+    let pnames = filter (`notElem` [".", ".."]) names
+    paths <- forM pnames $ \name -> do
+        let path = dir </> name
+        isDirectory <- doesDirectoryExist path
+        if isDirectory
+            then walkDirectory path
+            else return [path]
+
+    return $ concat paths
+
+    where
+        -- skip unreadable dirs
+        skipDir :: IOException -> IO [FilePath]
+        skipDir e = do
+            hPutStrLn stderr $ show e
+            return []
---
+++ README.md
@@ -0,0 +1,91 @@
+# S3
+
+Simple push/pull access to your Amazon S3 storage.
+
+### Goal
+
+The eventual goal it so provide a smart off-site backup tool utilizing 
+Amazon's S3 cloud storage. This tool would need to behave like rsync and 
+have a focus on cutting unnecessary data transfer (since you pay by the 
+byte).
+
+Currently one can use this tool to push files or directories up to a 
+bucket or pull files down from a bucket.
+
+### Configuration
+
+For now the project uses configuration by compilation. That means you'll 
+need to create a `Config.hs` before compiling the executable (mine is 
+hidden from the repo).
+
+Here is an example:
+
+~~~ { .haskell }
+module Config
+    ( myConnection
+    , myBucket
+    ) where
+
+import Network.AWS.AWSConnection
+
+myConnection :: AWSConnection
+myConnection = amazonS3Connection "<your key>" "<your secret key>"
+
+myBucket :: String
+myBucket = "<some bucket>"
+~~~
+
+### Installation
+
+The following packages are required (both available on hackage):
+
+~~~ 
+cabal install hs3
+cabal install wai-app-static
+~~~
+
+`wai-app-static` is only used for its `defaultMimeTypeByExt` function 
+(for now).
+
+Once you setup `Config.hs`, compile:
+
+~~~ 
+ghc --make -o s3 Main.hs
+~~~
+
+*Feel free to choose a different name for the binary*
+
+`mv ./s3` into `$PATH` or always call it qualified.
+
+### Usage
+
+Currently, there are two ways to use `s3`: *send* is the default and 
+*get* is used when you pass `--get` as the first argument.
+
+~~~ 
+usage: s3 [ --get ] <path> ...
+~~~
+
+You can pass as many `<path>` arguments as you would like. *Get*ting 
+directories is not supported yet (but *send*ing them is).
+
+In both cases, the S3 side of the transaction will use a stripped 
+filename while the filesystem side is always written/read exactly as it 
+was passed.
+
+Here are some examples to clarify:
+
+
+Path        Read-written on filesystem   Read-written on S3
+-----      ---------------------------  -------------------
+./foo                            ./foo         myBucket/foo
+../bar                          ../bar         myBucket/bar
+/some/dir                    /some/dir    myBucket/some/dir
+
+
+### Thanks
+
+Greg Heartsfield for [hs3][] and Michael Snoyman for [wai-app-static][].
+
+[hs3]:            http://hackage.haskell.org/package/hs3
+[wai-app-static]: http://hackage.haskell.org/package/wai-app-static
