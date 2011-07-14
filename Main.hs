2275e370a3876377aa505c2bb164a6ce0c95dfc2
patrick brisbin <pbrisbin@gmail.com>
2011-07-14 19:12:52 -0400

    begin rewrite

--- Main.hs
+++ Main.hs
@@ -1,8 +1,7 @@
--- https://github.com/pbrisbin/s3
 module Main where
 
-import Config
-
+import Network.AWS.AWSConnection
+import Network.AWS.AWSResult
 import Network.AWS.S3Object
 
 import Control.Exception  (IOException, handle)
@@ -17,61 +16,78 @@ import qualified Data.ByteString.Lazy       as B
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Char8      as C8
 
+type Bucket = String
+
+data Source = SBucket Bucket FilePath
+            | SLocal [FilePath]
+
+data Destination = DBucket Bucket FilePath
+                 | DLocal FilePath
+
 main :: IO ()
-main = handleArgs =<< getArgs
+main = undefined
 
-handleArgs :: [String] -> IO ()
-handleArgs []          = putStrLn "usage: s3 [ --get ] <path> ..."
-handleArgs args@(a:as) =
-    if a == "--get"
-        then mapM_ getArg  as
-        else mapM_ sendArg args
+s3cp :: Source -> Destination -> IO (AWSResult ())
 
-sendArg :: FilePath -> IO ()
-sendArg fp = do
-    isDirectory <- doesDirectoryExist fp
-    if isDirectory
-        then mapM_ sendFile =<< walkDirectory fp
-        else do
-            isFile <- doesFileExist fp
-            if isFile
-                then sendFile fp
-                else return ()
+-- Copy the bucket files to the local destination
+--
+-- todo: if bucket dest is a directory, copy it and it's contents
+--
+s3cp (SBucket b bfp) (DLocal fp) = do
+    mconn <- amazonS3ConnectionFromEnv
+    case mconn of
+        Just conn -> do
+            getFile conn b (strip bfp) fp
+            return $ Right ()
+        _         -> return $ Left awsEnvNotSet
 
--- AWS will 404 on directory argument
-getArg :: FilePath -> IO ()
-getArg = getFile
+-- Copy the local file(s) up to the bucket
+--
+-- todo: s3cp foo.txt bucket:foo.txt should create bucket/foo.txt
+--       s3cp foo.txt bucket:bar     should create bucket/bar/foo.txt
+--
+--       but there's no way to tell the user's intention
+--
+s3cp (SLocal fps) (DBucket b bfp) = do
+    mconn <- amazonS3ConnectionFromEnv
+    case mconn of
+        Just conn -> do
+            _ <- forM fps $ \fp -> do
+                let ffp = strip bfp </> fp
+                putFile conn b fp ffp
+            return $ Right ()
+        _ -> return $ Left awsEnvNotSet
 
-sendFile :: FilePath -> IO ()
-sendFile fp = handle skipFile $ do
+-- invalid usage, throw error
+s3cp _ _ = return $ Left awsInvalidArgs
+
+putFile :: AWSConnection -> Bucket -> FilePath -> FilePath -> IO ()
+putFile aws b fpFrom fpTo = handle skipFile $ do
     obj <- do
-        fileData <- B.readFile fp
+        fileData <- B.readFile fpFrom
         return S3Object
-            { obj_bucket   = myBucket
-            , obj_name     = strip fp
-            , content_type = C8.unpack $ defaultMimeTypeByExt fp
+            { obj_bucket   = b
+            , obj_name     = fpTo
+            , content_type = C8.unpack $ defaultMimeTypeByExt fpFrom
             , obj_headers  = []
             , obj_data     = fileData
             }
-
-    resp <- sendObject myConnection obj
+    resp <- sendObject aws obj
 
     case resp of
-        Left  e -> hPutStrLn stderr $ show e
-        Right _ -> putStrLn $ "Sent " ++ fp
-
-getFile :: FilePath -> IO ()
-getFile fp = handle skipFile $ do
-    resp <- getObject myConnection $ skel fp
+        Left e  -> hPutStrLn stderr $ show e
+        Right _ -> return () -- todo: -v option
+    
+getFile :: AWSConnection -> Bucket -> FilePath -> FilePath -> IO ()
+getFile aws b fpFrom fpTo = handle skipFile $ do
+    -- a skeleton object, identifies the file by bucket/path
+    let obj = S3Object b fpFrom "" [] (L8.pack "")
+    resp <- getObject aws obj
     case resp of
-        Left  e   -> hPutStrLn stderr $ show e
-        Right obj -> do
-            B.writeFile fp (obj_data obj)
-            putStrLn $ "Wrote " ++ fp
-
-    where
-        skel :: FilePath -> S3Object
-        skel f = S3Object myBucket (strip f) "" [] (L8.pack "")
+        Left e -> hPutStrLn stderr $ show e
+        Right ob -> do
+            B.writeFile fpTo (obj_data obj)
+            -- todo: -v option
 
 strip :: FilePath -> FilePath
 strip ('/':rest)         = strip rest
@@ -79,7 +95,6 @@ strip ('.':'/':rest)     = strip rest
 strip ('.':'.':'/':rest) = strip rest
 strip x                  = x
 
--- skip unreadable files
 skipFile :: IOException -> IO ()
 skipFile e = hPutStrLn stderr $ show e
 
@@ -102,3 +117,10 @@ walkDirectory dir = handle skipDir $ do
         skipDir e = do
             hPutStrLn stderr $ show e
             return []
+
+-- custom errors
+awsInvalidArgs :: ReqError
+awsInvalidArgs = AWSError "" "Arguments for copy are invalid"
+
+awsEnvNotSet :: ReqError
+awsEnvNotSet= AWSError "" "AWS environment variables are not set"
